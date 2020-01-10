16fd77c57e3c6d6b863e0846430a6800fa0a93d2
Maciej4 <m@maciej.me>
2020-01-09 21:54:49 -0800

    add test examples

--- src/main/java/frc/robot/controllers/NetworktablesInterface.java
+++ src/main/java/frc/robot/controllers/NetworktablesInterface.java
@@ -16,15 +16,18 @@ public class NetworktablesInterface {
     NetworkTable table = inst.getTable("limelight");
     NetworkTableEntry xEntry = table.getEntry("tx");
     NetworkTableEntry yEntry = table.getEntry("tx");
+    NetworkTableEntry robotHeartbeat = table.getEntry("robotHeartbeat");
+    // TODO: Find way to get limelight hearbeat to confirm connection
 
     public NetworktablesInterface(){
-        inst.startClientTeam(972);  // where TEAM=190, 294, etc, or use inst.startClient("hostname") or similar
+        inst.startClientTeam(972);
         inst.startDSClient();  // recommended if running on DS computer; this gets the robot IP from the DS
     }
 
     public void run(){
-        double x = xEntry.getDouble(0.0);
-        double y = yEntry.getDouble(0.0);
-        System.out.println("tx: " + x + " ty: " + y);
+        tx = xEntry.getDouble(0.0);
+        ty = yEntry.getDouble(0.0);
+        robotHeartbeat.setNumber(System.currentTimeMillis());
+        System.out.println("tx: " + tx + " ty: " + ty);
     }
 }
---
+++ src/test/java/frc/robot/util/SmoothDampTest.java
@@ -0,0 +1,30 @@
+package frc.robot.util;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import static org.mockito.Mockito.*;
+import edu.wpi.first.wpilibj.*;
+import com.revrobotics.CANSparkMax;
+
+public class SmoothDampTest {
+
+    @Test
+    public void smoothDampStepsize1() { 
+        // Arrange
+        // CANSparkMax motor = mock(CANSparkMax.class);
+        // Servo servo = mock(Servo.class);
+
+        // Act
+        SmoothDamp smoothDamp = new SmoothDamp(1);
+
+        // Assert
+        assertEquals(1, smoothDamp.stepSize, 0.1);
+    }
+
+    @Test
+    public void smoothDampStepsize10() {
+        SmoothDamp smoothDamp = new SmoothDamp(10);
+        
+        assertEquals(10, smoothDamp.stepSize, 0.1);
+    }
+}
