68cf18e836c110a072c9a869173025ca596d48c9
Maciej4 <m@maciej.me>
2020-01-11 14:41:44 -0800

    manual pull of vision alignment

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -69,6 +69,6 @@ public class Robot extends TimedRobot {
       turnPower = -maxTurn;
     }
 
-    Context.robotController.drivetrain.arcadeDrive(linearPower, turnPower);
+    // Context.robotController.drivetrain.arcadeDrive(linearPower, turnPower);
   }
 }
--- src/main/java/frc/robot/util/AdditionalMath.java
+++ src/main/java/frc/robot/util/AdditionalMath.java
@@ -1,6 +1,14 @@
 package frc.robot.util;
 
-public class Deadband {
+public class AdditionalMath
+{
+    public static double Clamp(double value, double min, double max)
+    {
+        if(value >= max) return max;
+        if(value <= min) return min;
+        return value;
+    }
+
     public static double handle (double input, double deadband)
     {
         if (Math.abs(input) < Math.abs(deadband))
