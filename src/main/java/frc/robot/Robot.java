881dfe192b61899898b2cec765dfe8eea6f615f7
Nikita Cheblokov <ninja11103@gmail.com>
2020-01-15 17:43:18 -0800

    Merge master

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,72 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import frc.robot.controllers.RobotController;
+import frc.robot.util.*;
+
+<<<<<<< HEAD
+public class Robot extends TimedRobot
+{
+=======
+public class Robot extends TimedRobot {
+  public RobotController robotController;
+  public DriverJoystick joy = new DriverJoystick();
+>>>>>>> master
+
+  public double origTime;
+  public double robotStartTime;
+
+  @Override
+  public void robotInit() {
+    Context.robotController = new RobotController();
+    robotStartTime = System.currentTimeMillis()/1000.0;
+  }
+
+  @Override
+  public void robotPeriodic() {
+
+  }
+
+  @Override
+  public void autonomousInit() {
+    origTime = System.currentTimeMillis();
+    Context.robotController.autoDrive.startSpline();
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    Context.robotController.autoDrive.loop((System.currentTimeMillis() - origTime)/1000);
+  }
+
+  @Override
+  public void teleopInit() {
+    Context.robotController.drivetrain.resetEncoders();
+  }
+
+  boolean align = false;
+  @Override
+  public void teleopPeriodic()
+  {
+    Context.robotController.ntInterface.run();
+    double joyX = -Context.ManagedJoystick.getAxisDeadBandManaged(0);
+    double joyY = -Context.ManagedJoystick.getAxisDeadBandManaged(1);
+
+    if(Context.ManagedJoystick.getRawButtonPressed(4))
+    {
+      Context.visionAllignment.RESET();
+      align = !align;
+    }
+    if(joyX != 0 || joyY != 0)
+    {
+      align = false;
+    }
+    if(align)
+    {
+      Context.visionAllignment.loop();
+    }
+    if(!align)
+    {
+    Context.robotController.drivetrain.arcadeDrive(joyY, joyX);
+    }
+  }
+}
