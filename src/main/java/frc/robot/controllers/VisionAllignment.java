8bf80ae5b759b73de53b16bae323beb0cfcb5785
Maciej4 <m@maciej.me>
2020-01-17 21:31:22 -0800

    rename statusEnum and fix access mods

--- src/main/java/frc/robot/controllers/VisionAllignment.java
+++ src/main/java/frc/robot/controllers/VisionAllignment.java
@@ -6,40 +6,41 @@ import frc.robot.util.PID;
 
 public class VisionAllignment
 {
-    final double headingP = 0.1;
-    final double headingI = 0.0;
-    final double headingD = 2.0;
+    private final double headingP = 0.1;
+    private final double headingI = 0.0;
+    private final double headingD = 2.0;
 
-    final double navXYawP = 0.1;
-    final double navXYawI = 0.0;
-    final double navXYawD = 2.0;
+    private final double navXYawP = 0.1;
+    private final double navXYawI = 0.0;
+    private final double navXYawD = 2.0;
 
-    public enum statusEnum { IDLE, IN_PROGRESS, ALIGNED, FAILED };
-    public statusEnum alignmentStatus = statusEnum.IDLE;
+    public enum StatusEnum { IDLE, IN_PROGRESS, ALIGNED, FAILED };
+    public StatusEnum alignmentStatus = StatusEnum.IDLE;
 
-    public double tx;
-    public double ty;
+    public double tx = 0;
+    public double ty = 0;
+
+    public double oldTx = 0.0;
+    public double newTx = 0.0;
 
     /////////////////////////////////////IMPORTANT////////////////////////////////////////////// oh, you're changing these during a competition?
     public PID headingPID = new PID(headingP, headingI, headingD); // PID used when optical aquisition is avalilable
     public PID navXYawPID = new PID(navXYawP, navXYawI, navXYawD); // PID used when no optical aquisition is avalilable and we need to rely on NavX data
     /////////////////////////////////////IMPORTANT//////////////////////////////////////////////
 
-    double pastTime = System.currentTimeMillis()-20;
-    double currentTime = System.currentTimeMillis();
+    public double pastTime = System.currentTimeMillis()-20;
+    public double currentTime = System.currentTimeMillis();
 
-    double frameAngleDelta = 0.0;
-    double oldTx = 0.0;
-    double newTx = 0.0;
+    public double frameAngleDelta = 0.0;
 
-    double timeoutCounter = 0.0;
+    public double timeoutCounter = 0.0;
 
-    boolean timedOut = false;
+    public boolean timedOut = false;
 
-    double rotationLocalized;
-    double navXYawOffset = 0.0;
+    public double rotationLocalized;
+    public double navXYawOffset = 0.0;
 
-    boolean targetFound= false;
+    public boolean targetFound= false;
 
     public void loop()
     {
@@ -48,10 +49,10 @@ public class VisionAllignment
         System.out.println("Alignment status: " + alignmentStatus.toString());
         System.out.println(timeoutCounter);
 
-        if(alignmentStatus == statusEnum.IN_PROGRESS || alignmentStatus == statusEnum.IDLE)
+        if(alignmentStatus == StatusEnum.IN_PROGRESS || alignmentStatus == StatusEnum.IDLE)
         {
             System.out.println("Running Alignment");
-            alignmentStatus = statusEnum.IN_PROGRESS;
+            alignmentStatus = StatusEnum.IN_PROGRESS;
 
             double offset = AdditionalMath.OvercomeFriction(headingPID.update(0.0, tx, currentTime-pastTime), Context.ckStatic); // integrating the PID for allignment
             double drivePower = AdditionalMath.Clamp(offset, -Context.maxTurnPower, Context.maxTurnPower); // clamping the value for safety reasons and concerns
@@ -68,14 +69,14 @@ public class VisionAllignment
 
             if(Math.abs(tx) <= Context.alignmentThreshold && targetFound) // if the robot is close enough to the target, stop aligning
             {
-                alignmentStatus = statusEnum.ALIGNED;
+                alignmentStatus = StatusEnum.ALIGNED;
             }
             if(Math.abs(tx) >= Context.alignmentThreshold && timedOut) // if the robot times out and is not within acceptable, fail alignment 
             {// use cases: robot gets pinned in such a way that the camera still sees the target, but can not move, therefore the tx does not change
-                alignmentStatus = statusEnum.FAILED;
+                alignmentStatus = StatusEnum.FAILED;
             }
         }
-        if(alignmentStatus == statusEnum.ALIGNED)
+        if(alignmentStatus == StatusEnum.ALIGNED)
         {
             keepTrack(); // once the target is aquired, keep track of it, in case we get nudged around
         }
@@ -96,13 +97,13 @@ public class VisionAllignment
 
     public boolean isAligned()
     {
-        if(alignmentStatus == statusEnum.ALIGNED) { 
+        if(alignmentStatus == StatusEnum.ALIGNED) { 
             return true;
         }
         return false;
     }
 
-    public statusEnum getAlignmentStatus()
+    public StatusEnum getAlignmentStatus()
     {
         return alignmentStatus;
     }
@@ -145,7 +146,7 @@ public class VisionAllignment
 
     public void RESET() // resets the class, ready for the next alignment
     {
-        alignmentStatus = statusEnum.IDLE;
+        alignmentStatus = StatusEnum.IDLE;
         rotationLocalized = 0.0;
         pastTime = System.currentTimeMillis()-20;
         currentTime = System.currentTimeMillis();
