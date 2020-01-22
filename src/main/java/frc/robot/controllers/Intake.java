ad05daf869875ecbc29f501b78f9a0eb8123a562
Pranav <tadp3975@lgsstudent.org>
2020-01-21 17:11:34 -0800

    Color Sensor
    Added color sensor library and working on porting over code

--- src/main/java/frc/robot/controllers/Intake.java
+++ src/main/java/frc/robot/controllers/Intake.java
@@ -1,18 +1,36 @@
 package frc.robot.controllers;
 
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorMatch;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Joystick;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.util.Color;
-
 import frc.robot.util.*;
 
 public class Intake
 {
+    private final I2C.Port i2cPort = I2C.Port.kOnboard;
+    private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+    private final ColorMatch m_colorMatcher = new ColorMatch();
+    Color detectedColor = m_colorSensor.getColor();
+
+    private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+    private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+    private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+    private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+
+    m_colorMatcher.addColorMatch(kBlueTarget);
+
     Joystick joy = new Joystick(0);
     TalonSRX rollingIntake = new TalonSRX(0);
     TalonSRX beltIntake = new TalonSRX(1);
