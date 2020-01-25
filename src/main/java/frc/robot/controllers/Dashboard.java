561a0a1bc53e0a324678e3e97b3ac16ba6dbdd3a
Me1234q <me1234q@gmail.com>
2020-01-24 16:05:41 -0800

    Nothing

--- src/main/java/frc/robot/controllers/Dashboard.java
+++ src/main/java/frc/robot/controllers/Dashboard.java
@@ -1,13 +1,20 @@
 package frc.robot.controllers;
 
-import edu.wpi.first.wpilibj.smartdashboard.*;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.*;
 
 public class Dashboard {
+    private ShuffleboardTab tab = Shuffleboard.getTab("SmartDashboard");
+
+    private final NetworkTableEntry[] entries = new NetworkTableEntry[] {
+        NetworkTableEntry
+    };
+
     public Dashboard() {
         
     }
 
     public static void putJoystick(double value) {
-        SmartDashboard.putNumber("Joystick", value);
+        
     }
 }
