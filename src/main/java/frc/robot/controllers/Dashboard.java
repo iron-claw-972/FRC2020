99bb05ffc03e7907dc1b1f91308d53f8599f93a7
Me1234q <me1234q@gmail.com>
2020-01-28 16:53:16 -0800

    Fixed Properties

--- src/main/java/frc/robot/controllers/Dashboard.java
+++ src/main/java/frc/robot/controllers/Dashboard.java
@@ -19,17 +19,21 @@ public class Dashboard {
 
         drivetrainLayout = tab.getLayout("Drivetrain", BuiltInLayouts.kGrid)
             .withPosition(0,0)
-            .withSize(2,5)
-            .withProperties(Map.of("Number of rows", 2, "number of columns", 2, "Label Position", "TOP"));
+            .withSize(3,3)
+            .withProperties(Map.of("Number of rows", 2))
+            .withProperties(Map.of("Number of columns", 2))
+            .withProperties(Map.of("Label Position", "TOP"));
 
         leftEncoderEntry = drivetrainLayout.add("Left", 0)
             .withPosition(0,0)
+            .withSize(2,1)
             .withWidget(BuiltInWidgets.kNumberSlider)
             .withProperties(Map.of("min", -1, "max", 1))
             .getEntry();
 
         rightEncoderEntry = drivetrainLayout.add("Right", 0)
             .withPosition(1,0)
+            .withSize(2,1)
             .withWidget(BuiltInWidgets.kNumberSlider)
             .withProperties(Map.of("min", -1, "max", 1))
             .getEntry();
@@ -37,6 +41,6 @@ public class Dashboard {
 
     public static void putJoystick(Joystick joy) {
         leftEncoderEntry.setDouble(joy.getRawAxis(0));
-        rightEncoderEntry.setDouble(joy.getRawAxis(4));
+        rightEncoderEntry.setDouble(joy.getRawAxis(2));
     }
 }
