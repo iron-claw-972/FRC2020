6d7df6714fc7b9cf5220c669d188c87b6cbe56a5
ColeFoster08 <cole.foster@pseudoworld.com>
2020-02-05 18:13:30 -0800

    created possible climg code

--- src/main/java/frc/robot/controllers/Climber.java
+++ src/main/java/frc/robot/controllers/Climber.java
@@ -6,12 +6,26 @@ import frc.robot.util.*;
 
 public class Climber
 {
-    TalonSRX coilMotor;
+    public TalonSRX coilMotor;
+    public CANSparkMax telescopeMotor;
     PID liftPID;
+    long pastTime;
+    double desiredPosition;
+
 
     public Climber(){
         coilMotor = new TalonSRX (Context.climberMotorID);
+        telescopeMotor = new CANSparkMax(Context.climberMotorID, MotorType.kBrushless);
         liftPID = new PID(1, 0, 0); //Not tuned & not used
+        pastTime = System.currentTimeMillis();
+
+    }
+    public void telescopeUp(double PIDVal) {
+        telescopeMotor.set(ControlMode.PercentOutput, PIDVal);
+    }
+
+    public void telescopeDown(double PIDVal) {
+        telescopeMotor.set(ControlMode.PercentOutput, PIDVal);
     }
 
     public void coil(){
@@ -20,5 +34,24 @@ public class Climber
 
     public void uncoil(){
         coilMotor.set(ControlMode.PercentOutput, Context.uncoilSpeed);
+    }
+
+    public void loop() {
+        double currentPosition = telescopeMotor.getPosition();
+
+        long currentTime = System.currentTimeMillis();
+        double deltaTime = currentTime - pastTime;
+        if (Context.robotController.driverJoystick.getClimbU())
+        {
+            desiredPosition = 1;
+            telescopeUp(liftPID.update(desiredPosition, currentPosition, deltaTime));
+        }
+        else if (Context.robotController.driverJoystick.getClimbD())
+        {
+            desiredPosition = 0;
+            telescopeDown(liftPID.update(desiredPosition, currentPosition, deltaTime));
+            coil();
+        }
+        pastTime = currentTime;
     }
 }
