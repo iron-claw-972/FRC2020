8bc32d75d90d16741865ce952085e202dacf34b1
Maciej4 <m@maciej.me>
2020-02-09 11:01:46 -0800

    clean up code

--- src/main/java/frc/robot/controllers/NMFColorSensorController.java
+++ src/main/java/frc/robot/controllers/NMFColorSensorController.java
@@ -20,9 +20,9 @@ public class NMFColorSensorController {
     public int currentPosition = 0;
     public boolean[] ballPositions = {false, false, false, false, false};
     public boolean currentSectorYellow = false;
-  
+
     private final ColorSensorV3 m_colorSensor;
-   
+
     /**
      * A Rev Color Match object is used to register and detect known colors. This can 
      * be calibrated ahead of time or during operation.
@@ -31,88 +31,79 @@ public class NMFColorSensorController {
      * with given confidence range.
      */
     private final ColorMatch m_colorMatcher = new ColorMatch();
-   
+
     public NMFColorSensorController(){
-        m_colorSensor = new ColorSensorV3(i2cPort);
-
+      m_colorSensor = new ColorSensorV3(i2cPort);
     }
+
     public boolean isYellow(double r, double g, double b){
-        if (r<.4 && b<.16){
-          return true;
-    
-        }
-        else{
-          return false;
-        }
-    
+      if (r<.4 && b<.16) {
+        return true;
+      } else {
+        return false;
       }
-    
-      public boolean isBlue(double r, double g, double b){
-        if (b>.3){
-          return true;
-        }
-        else{
-          return false;
-        }
-    
-      }
+    }
 
-      public void loop(){
-        Color detectedColor = m_colorSensor.getColor();
-        //System.out.println(detectedColor.red +" "+ detectedColor.green + " "+ detectedColor.blue);
-        if (isYellow(detectedColor.red, detectedColor.green, detectedColor.blue)){
-          currentColor = "yellow";
+    public boolean isBlue(double r, double g, double b){
+      if (b>.3) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    public void loop(){
+      Color detectedColor = m_colorSensor.getColor();
+      //System.out.println(detectedColor.red +" "+ detectedColor.green + " "+ detectedColor.blue);
+      if (isYellow(detectedColor.red, detectedColor.green, detectedColor.blue)){
+        currentColor = "yellow";
+      }
+      else{
+        if (isBlue(detectedColor.red, detectedColor.green, detectedColor.blue)){
+            currentColor = "blue";
         }
         else{
-          if (isBlue(detectedColor.red, detectedColor.green, detectedColor.blue)){
-              currentColor = "blue";
-          }
-          else{
-             currentColor = "none";
-          }
-         
-        }
-        if (currentColor!=previousColor){
-          if (currentPosition==5){ currentPosition=0;}
-          switch (currentColor){
-            case ("blue"): {
-              if (currentPosition<5){currentPosition++;}
-              else{currentPosition=0;}
-              currentSectorYellow = false;
-              break;
-              
-            }
-            case ("yellow"): {
-              //System.out.println(currentColor);
-              if (currentPosition==5){ currentPosition=0;}
-              ballPositions[currentPosition] = true;
-              currentSectorYellow = true;
-              break;
-              
-            }
-            case("none"):{
-              if (currentPosition==5){ currentPosition=0;}
-              if (!currentSectorYellow){
-                ballPositions[currentPosition] = false;
-              }
-              break;
-              
-            }
-        }
-          for(boolean bool : ballPositions) {
-            System.out.print(bool + " ");
-          }
-          System.out.println();
+            currentColor = "none";
         }
         
-    
-        
-    
-        previousColor = currentColor;
-    
       }
+      if (currentColor!=previousColor){
+        if (currentPosition==5){ currentPosition=0;}
+        switch (currentColor){
+          case ("blue"): {
+            if (currentPosition<5){currentPosition++;}
+            else{currentPosition=0;}
+            currentSectorYellow = false;
+            break;
+            
+          }
+          case ("yellow"): {
+            //System.out.println(currentColor);
+            if (currentPosition==5){ currentPosition=0;}
+            ballPositions[currentPosition] = true;
+            currentSectorYellow = true;
+            break;
+            
+          }
+          case("none"):{
+            if (currentPosition==5){ currentPosition=0;}
+            if (!currentSectorYellow){
+              ballPositions[currentPosition] = false;
+            }
+            break;
+            
+          }
+      }
+        for(boolean bool : ballPositions) {
+          System.out.print(bool + " ");
+        }
+        System.out.println();
+      }
+      
+      previousColor = currentColor;
+    }
 
-      public boolean[] getBallPositions(){
-          return ballPositions;
-      }
+    public boolean[] getBallPositions(){
+        return ballPositions;
+    }
 }
