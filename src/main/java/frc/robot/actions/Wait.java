cfb3f4cdf6dff0bba1c3fe108da9dffc41c59475
Maciej4 <m@maciej.me>
2020-02-15 20:22:20 -0800

    fix wait.java

--- src/main/java/frc/robot/actions/Wait.java
+++ src/main/java/frc/robot/actions/Wait.java
@@ -3,6 +3,11 @@ package frc.robot.actions;
 public class Wait extends Action{
     public long duration;
 
+    public Wait (long duration)
+    {
+        this.duration = duration;
+    }
+
     public void loop()
     {
         if(System.currentTimeMillis() - startTime > duration)
