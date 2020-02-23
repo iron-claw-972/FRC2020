548b9b073e5d73e43ba71027df746bcc0b6a2ee9
Maciej4 <m@maciej.me>
2020-02-22 20:38:54 -0800

    add comment in gson
    The comment explains the use of the GSON library in order to make a deep copy of the action instance.

--- src/main/java/frc/robot/util/Trigger.java
+++ src/main/java/frc/robot/util/Trigger.java
@@ -14,9 +14,13 @@ public class Trigger {
         gson = new Gson();
     }
 
-    public void loop()
-    {
+    public void loop() {
         if(Context.robotController.driverJoystick.getButtonPressed(buttonID)){
+            // The action is serialized to json and deselerialized in order to create a deep copy,
+            // which is necessary in order to not edit the action passed to the trigger in the
+            // trigger's initilzation. Without doing this, the trigger's action would be edited
+            // and if the trigger were to be activated again, it would result in the addition
+            // of a already complete action to the scheduler.
             String seralized = gson.toJson(this.action);
             Action clone = gson.fromJson(seralized, action.getClass());
             Context.robotController.parallelScheduler.add(clone);
