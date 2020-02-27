6ff56a05c167a122093df6570330482fec068943
ColeFoster08 <cole.foster@pseudoworld.com>
2020-02-26 16:24:38 -0800

    added 2 basic tests to climber test

--- src/test/java/frc/robot/controllers/ClimberTest.java
+++ src/test/java/frc/robot/controllers/ClimberTest.java
@@ -2,16 +2,19 @@ package frc.robot.controllers;
 
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import org.junit.*;
 import com.revrobotics.CANSparkMax;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.*;
 
 public class ClimberTest
 {
     // Declaration of variables used to store inputs to motors
     public double telescopePower = 0;
+    public double coilPower = 0;
 
     // Creation of mock sparks and putting them into NeoDrivetrain
     public CANSparkMax telescope = mock(CANSparkMax.class);
@@ -28,13 +31,27 @@ public class ClimberTest
             telescopePower = power.doubleValue();
             return null;
         }).when(telescope).set(any(Double.class));
+
+        doAnswer(invocation -> {
+            Double power = invocation.getArgument(1, Double.class);
+            coilPower = power.doubleValue();
+            return null;
+        }).when(coil).set(eq(ControlMode.PercentOutput), any(Double.class));
     }
 
-    // Simple test that calls method that calls in turn the .set function of motors
+    // Test that ensures that coil method increases coil motor power to 0.5
     @Test
-    public void climberTest() {
-        assertEquals(1.0, 1, 0.1);
-
+    public void coilUpTest() {
+        climb.coil();
+        double finalValue = coilPower;
+        assertEquals(finalValue, 0.5, 0.1);
     }
-    
+
+    // Test that ensures that coil method decreases coil motor power to -0.5
+    @Test
+    public void coilDownTest() {
+        climb.uncoil();
+        double finalValue = coilPower;
+        assertEquals(finalValue, -0.5, 0.1);
+    }
 }
