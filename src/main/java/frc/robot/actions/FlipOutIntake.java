990bbe60fec030dc004ed7c365ab07b2e9851847
Me1234q <me1234q@gmail.com>
2020-03-03 17:10:07 -0800

    Intake Actions

---
+++ src/main/java/frc/robot/actions/FlipOutIntake.java
@@ -0,0 +1,20 @@
+package frc.robot.actions;
+
+import frc.robot.util.*;
+
+public class FlipOutIntake extends Action {
+
+    public void start() {
+        super.start();
+        
+        Context.robotController.intake.flipOut();
+        Context.robotController.nmfController.spinNMFIntaking();
+
+        markComplete();
+    }
+
+    public void loop()
+    {
+        
+    }
+}
