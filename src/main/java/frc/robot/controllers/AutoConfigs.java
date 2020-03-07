5992fe0a2db132917cad73e4c77dacbfca78d756
Me1234q <me1234q@gmail.com>
2020-03-06 16:41:39 -0800

    Basic Configurations

--- src/main/java/frc/robot/controllers/AutoConfigs.java
+++ src/main/java/frc/robot/controllers/AutoConfigs.java
@@ -4,16 +4,17 @@ import frc.robot.actions.*;
 import frc.robot.actions.auto.*;
 
 public enum AutoConfigs {
-    /*
-    CENTER: On initiation line directly in front of power port
-    ALLIANCE_TRENCH: On initiation line in line with the center of your alliance's trench
-    OPPONENT_TRENCH: On initiation line in line with the center of your opponent's trench
 
-    BASIC: SHOOT 3 BALLS, then move off initiation line
-    */
-
-    CENTER_BASIC(
-        //new VisionAlign(),
+    POWER_PORT_BASIC(
+        new VisionAlign(),
+        new Forward(1.0)
+    ),
+    ALLIANCE_TRENCH_BASIC(
+        new VisionAlign(),
+        new Forward(1.0)
+    ),
+    MIDDLE_FIELD_BASIC(
+        new VisionAlign(),
         new Forward(1.0)
     );
 
