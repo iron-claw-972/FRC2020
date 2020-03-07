c9aef17853bc187295d7e380972435731e6c9355
whackamadoodle3000 <orca.pranav@gmail.com>
2020-03-06 22:13:29 -0800

    added triggers for new actions

--- src/main/java/frc/robot/util/Context.java
+++ src/main/java/frc/robot/util/Context.java
@@ -54,12 +54,13 @@ public class Context {
     public static final int quickTurnLeftTriggerID = 2;
     public static final int quickTurnRightTriggerID = 3;
     public static final int loopyLoopBreak = 7;
+
     public static final int flipOutIntakeButtonID = 5;
     public static final int flipInIntakeButtonID = 6;
-    public static final int reverseNMFDirectionButtonID = -1;
-    public static final int reverseIntakeDirectionButtonID = -1;
+    public static final int reverseNMFDirectionButtonID = 3;
+    public static final int reverseIntakeDirectionButtonID = 1;
     public static final int spinIntakeTriggerID = 2;
-    public static final int spinNMFToggleButtonID = 4;
+    public static final int spinNMFToggleTriggerID = 3;
 
 
 
