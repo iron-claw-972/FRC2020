7c02dd1c6b198ad43e0ebb10a224d339d7e5e92b
whackamadoodle3000 <orca.pranav@gmail.com>
2020-03-06 17:55:03 -0800

    more

--- src/main/java/frc/robot/util/Context.java
+++ src/main/java/frc/robot/util/Context.java
@@ -56,8 +56,8 @@ public class Context {
     public static final int loopyLoopBreak = 7;
     public static final int flipOutIntakeButtonID = 5;
     public static final int flipInIntakeButtonID = 6;
-    public static final int reverseNMFDirectionButtonID = -1;
-    public static final int reverseIntakeDirectionButtonID = -1;
+    public static final int reverseNMFDirectionButtonID = 2;
+    public static final int reverseIntakeDirectionButtonID = 3;
     public static final int spinIntakeTriggerID = 2;
     public static final int spinNMFToggleButtonID = 4;
 
